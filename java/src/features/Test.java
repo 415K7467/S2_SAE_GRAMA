9c650b94d0e94b986617d584984df60e9a9f39c2
Vincent Chavot--Dambrun <vincent.chavot----dambrun@etu.univ-lyon1.fr>
2022-05-18 10:36:02 +0200

    fix : replace list of node in Hashmap

--- java/src/features/Test.java
+++ java/src/features/Test.java
@@ -1,13 +1,12 @@
 package features;
 
-import java.util.ArrayList;
-
 import dataImport.*;
-import HCI.*;
+//import HCI.*;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        CSVReader.extractData();        //put data in Nodes, a HashMap of Nodes and put Edges in ArrayLists in some Nodes
-
+        Nodes allnodes = CSVReader.extractData();        //put data in Nodes, a HashMap of Nodes and put Edges in ArrayLists in some Nodes
+        System.out.println(allnodes.getNode("Lyon 5")); //search for a Node by name and print it
+        System.out.println( allnodes.getNode("Lyon 5").neighborTwoDistance());
     }
 }
