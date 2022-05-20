a598d39e1bafca5c3ed31b302e5b606b70e41b81
Adrien TACHER <tacheradrien@gmail.com>
2022-05-20 08:49:14 +0200

    feat dijkstra

--- java/src/features/Dijkstra.java
+++ java/src/features/Dijkstra.java
@@ -1,2 +1,33 @@
-package features;public class Dijkstra {
+package features;
+
+import java.util.ArrayList;
+
+public class Dijkstra {
+
+    private Nodes nodeList;
+    private Node startNode;
+    private Node arrivalNode;
+
+    public Dijkstra(Nodes nodeList, Node startNode, Node arrivalNode) {
+        this.nodeList = nodeList;
+        this.startNode = startNode;
+        this.arrivalNode = arrivalNode;
+    }
+
+    public Nodes getNodeList() {
+        return nodeList;
+    }
+
+    public Node getStartNode() {
+        return startNode;
+    }
+
+    public Node getArrivalNode() {
+        return arrivalNode;
+    }
+
+    //Dijkstra's algorithm
+    private ArrayList<Node> dijkstra() {
+        return null;
+    }
 }
--- java/src/features/Test.java
+++ java/src/features/Test.java
@@ -3,11 +3,14 @@ package features;
 import java.util.ArrayList;
 
 import dataImport.*;
-import HCI.*;
+//import HCI.*;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        CSVReader.extractData();        //put data in Nodes, a HashMap of Nodes and put Edges in ArrayLists in some Nodes
+        Nodes nodes = CSVReader.extractData();        //put data in Nodes, a HashMap of Nodes and put Edges in ArrayLists in some Nodes
 
+
+        Node depart = new all.get("lyon 5");
+        Dijkstra dijkstra = new Dijkstra(nodes, );
     }
 }
